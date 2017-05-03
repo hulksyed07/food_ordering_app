6c2d1e591be13a3cda6ac3eef95c1f7d8df143b8
hulksyed07 <hulk.syed07@gmail.com>
2017-05-03 21:48:57 +0530

    changed the name of logo and added rspec basic files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([:name 'Indian'], [name: 'Chinese'], [name: 'Desserts'], [name: 'Starters'],[name: 'Appetizers'],[name: 'Cakes'])
